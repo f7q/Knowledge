50def35ecd39a0897eb0bf4be4248b63f6905fcd
USER001 F <USER001 F>
2017-02-12 09:29:23 +0900

    Add Test Sample and CakeBuild

---
+++ cakebuild/test/Sample.Tests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace Sample.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1() 
+        {
+			var str = new Lib().SayHello();
+            Assert.True(str.Equals("Say Hello"));
+        }
+    }
+}

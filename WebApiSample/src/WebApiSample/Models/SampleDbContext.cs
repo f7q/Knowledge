4384bb4ecc7313bd0018d49864b4fd2620de878e
USER001 F <USER001 F>
2017-02-16 01:58:54 +0900

    [WIP] Web API

---
+++ WebApiSample/src/WebApiSample/Models/SampleDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiSample.Models
+{
+    public class SampleDbContext : DbContext
+    {
+        public SampleDbContext(DbContextOptions<SampleDbContext> options) :base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured) return;
+            
+            optionsBuilder.UseSqlite("Data Source=..\\..\\..\\db.sqliteb");
+            
+        }
+
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>().HasKey(m => m.Id);
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ WebApiSample/src/WebApiSample/Startup.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Swashbuckle.Swagger.Model;
+using WebApiSample.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiSample
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env, IServiceProvider srv)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+            srv.GetService<ILoggerFactory>().AddConsole(Configuration.GetSection("Logging"));
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging();
+            services.AddEntityFrameworkSqlite();
+            services.AddDbContext<SampleDbContext>(options =>
+            {
+                options.UseSqlite(Configuration["Data:DefaultConnection:ConnectionString"]);
+            });
+            // Add framework services.
+            services.AddMvc();
+            services.AddSwaggerGen();
+            services.ConfigureSwaggerGen(options =>
+            {
+                options.SingleApiVersion(new Info
+                {
+                    Version = "v1",
+                    Title = "Geo Search API",
+                    Description = "A simple api to search using geo location in Elasticsearch",
+                    TermsOfService = "None"
+                });
+                //options.IncludeXmlComments(pathToDoc);
+                options.DescribeAllEnumsAsStrings();
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            //loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUi();
+        }
+    }
+}

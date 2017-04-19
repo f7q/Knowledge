34ca284db9b74fd815971463c260aff32ab23af3
USER001 F <USER001 F>
2017-04-19 23:27:44 +0900

    Add Dapper and Process.Kill
    - Web Api Reboot Sample[WIP]
    - DapperSample use EntitiyFrameworkCore

---
+++ DapperSample/src/DapperSample/Models/InMemoryDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiSample.Models
+{
+    public class InMemoryDbContext : DbContext
+    {
+        public InMemoryDbContext(DbContextOptions<InMemoryDbContext> options) :base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>().HasKey(m => m.Id);
+            base.OnModelCreating(builder);
+        }
+        
+    }
+}
---
+++ WebApiRebootSample/src/WebApiRebootSample/Controllers/ValuesController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebApiRebootSample.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private IApplicationLifetime _life;
+        public ValuesController(IApplicationLifetime life)
+        {
+            this._life = life;
+        }
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        // GET api/safeRestart
+        [HttpGet]
+        [Route("safeRestart")]
+        public void GetSafeRestart()
+        {
+            var process = Process.GetCurrentProcess();
+            /*var processId = process.Id;
+            using (var newprocess = new Process())
+            {
+                var startinfo = process.StartInfo;
+                Console.WriteLine(@"running...?{0}", process.ExitCode);
+                newprocess.StartInfo = startinfo;
+                process.Refresh();
+                Console.WriteLine(@"stop {0}", process.ExitCode);
+                process.Kill();
+                //process.WaitForExit();
+                //Environment.Exit(0);
+                //Program.Main();
+                if (process.HasExited)
+                {
+                    newprocess.Start();
+                    Console.WriteLine(@"restart {0}", process.ExitCode);
+                }
+            }*/
+            //this._life.StopApplication();
+            //this._life.ApplicationStarted;
+
+            return ;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
